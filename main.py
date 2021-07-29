638a7922e7094c326228c5c0b85bef2d10117977
Sergey Sidorov <sergey.sidorov@ugasoft.com>
2021-07-29 09:28:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import re
+
+FULL_TAG_PATTERN_EXPRESSION = "['a-z]+:[^,)]*"
+PREFIX_TAG_PATTERN_EXPRESSION = "TAG."
+USED_DELIMITER = ','
+FILE_NAME = "dbcreate.sql"
+TABLE_NAME = "all_tags"
+DEFAULT_ROLES = "'[\"admin\"]'"
+
+
+def createInsertScript(tag_info, table_name):
+    with open("insertTags.sql", "w") as fw:
+        i = 0
+        elements_count = len(tag_info)
+        while i < elements_count:
+            print("INSERT INTO " + table_name + " VALUES(" + str(tag_info[i]) + "," + str(tag_info[i + 1]) + "," +
+                  str(tag_info[i + 2]) + ");")
+            fw.write("INSERT INTO " + table_name + " VALUES(" + str(tag_info[i]) + "," + str(tag_info[i + 1]) + "," +
+                     str(tag_info[i + 2]) + ");\n")
+            i = i + 3
+
+
+def getTagsInfo(file_name):
+    tags_info = []
+    with open(file_name, 'r') as fileReader:
+        file_info = fileReader.read()
+        search_result = re.findall(FULL_TAG_PATTERN_EXPRESSION, file_info)
+        for tag in search_result:
+            tag_name = "'" + tag[7:]
+            tags_info.append(tag_name)
+            tags_info.append(getDataTypeCode(tag[1]))
+            tags_info.append(DEFAULT_ROLES)
+    return tags_info
+
+
+def getDataTypeCode(data_type_character):
+    if data_type_character == "s":
+        return 54
+    elif data_type_character == "d":
+        return 18
+    elif data_type_character == "i":
+        return 15
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    tagInfo = getTagsInfo(FILE_NAME)
+    createInsertScript(tagInfo, TABLE_NAME)
